0f97f820986e71c4e73e284a7066eb9add5cb2c6
Shreyas Prabhakar <148508046+shrprabh@users.noreply.github.com>
2026-03-03 13:45:56 -0600

    Fix import statement for ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components"; 
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 import Navbar from "./components/Navbar";
